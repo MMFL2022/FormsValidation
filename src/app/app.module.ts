a2ee1c4affdd42258e59729ad076ccb330911746
MMFL2022 <miller.mike.fl@gmail.com>
2023-10-16 13:52:55 -0400

    Trying to get the project to install dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from "./app.component";
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule
   ],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+//import { enableProdMode } from '@angular/core';
 
-if (environment.production) {
-  enableProdMode();
-}
+import { AppModule } from './app/app.module';
+//import { environment } from './environments/environment';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
