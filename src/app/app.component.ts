116813e9193275fb000592567caad00929351999
MMFL2022 <miller.mike.fl@gmail.com>
2023-10-16 15:23:49 -0400

    Application and test validation field is working...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 
 @Component({
@@ -8,4 +9,20 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   
+  testFormGroup: FormGroup;
+
+  testFieldAModel: string = '';
+  duplicateName: boolean = false;
+
+  constructor() {
+    this.testFormGroup = new FormGroup({
+      'testFieldAInput': new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.maxLength(64),
+          Validators.pattern('[a-zA-Z]+')
+        ]
+      }),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from "./app.component";
+import { TranslateLoader, TranslateModule, TranslateService } from "@ngx-translate/core";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { CookieService } from "./app/core/services/cookie.service";
+
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -13,11 +21,67 @@ import { AppComponent } from "./app.component";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [
+          HttpClient
+        ]
+      }
+    })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFactory,
+      deps: [
+        TranslateService,
+        CookieService
+      ],
+      multi: true
+    },
+    { provide: LOCALE_ID, useValue: 'en' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor() { }
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+//https://mcvendrell.medium.com/configuring-ngx-translate-to-load-at-startup-in-angular-1995e7dd6fcc
+export function appInitializerFactory(translateService: TranslateService, cookieService: CookieService) {
+  return () => {
+    translateService.addLangs(['en', 'en-GB', 'fr', 'fr-CA', 'es', 'es-US', 'de', 'pl']);
+    translateService.setDefaultLang('en');
+
+    if (cookieService.getCookie('acceptedCookies').valueOf() === 'true') {
+      if (translateService.langs.includes(cookieService.getCookie('language').valueOf())) {
+        translateService.use(cookieService.getCookie('language').valueOf()).subscribe();
+      } else {
+        cookieService.setCookie('language', 'en', 1000, '/');
+        translateService.use('en').subscribe();
+      }
+    } else {
+      let defaultLang = 'en';
+      let browserLang = translateService.getBrowserLang();
+
+      if (browserLang) {
+        defaultLang = browserLang;
+      }
+      
+      if (translateService.langs.includes(defaultLang)) {
+        translateService.use(defaultLang).subscribe();
+      }
+    }
+  };
 }
